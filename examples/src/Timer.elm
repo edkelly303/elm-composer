79bafe19797629b66135eb8ecfb143367c197979
Ed <edwardjkelly@gmail.com>
2024-10-13 13:28:49 +0100

    Tweaks to Timer example

--- examples/src/Timer.elm
+++ examples/src/Timer.elm
@@ -16,13 +16,15 @@ main :
         ( AppModel, ( TimerModel, () ) )
         ( Maybe AppMsg, ( Maybe TimerMsg, () ) )
 main =
-    Browser.element program
-
-
-program =
     Composer.Element.defineApp app
-        |> Composer.Element.addComponent (timerComponent { timerExpired = TimerExpired, timerReset = TimerReset })
+        |> Composer.Element.addComponent
+            (timerComponent
+                { timerExpired = TimerExpired
+                , timerReset = TimerReset
+                }
+            )
         |> Composer.Element.done
+        |> Browser.element
 
 
 type alias AppModel =
@@ -52,46 +54,54 @@ app =
                 [ Html.p []
                     [ Html.text
                         """
-                            The timer element below comes from an 
-                            encapsulated component, which manages its own 
-                            state, completely separately from the app's 
-                            model. It can send messages to the app's update 
-                            function, and receive messages sent by the app's 
-                            view function.
-                            """
+                        The timer element below comes from an 
+                        encapsulated component, which manages its own 
+                        state, completely separately from the main app's 
+                        model.
+                        """
                     ]
                 , timer.view
                 , Html.p []
                     [ Html.text
                         """
-                            Here is a `Debug.toString` of the app's model:
-                            """
+                        Here is a `Debug.toString` of the app's model:
+                        """
                     ]
                 , Html.p [ Html.Attributes.style "font-family" "monospace" ]
                     [ Html.text (Debug.toString model) ]
                 , Html.p []
                     [ Html.text
                         """
-                            The only thing the app's model does is keep track of
-                            whether the timer has expired. But as you can see, 
-                            it doesn't (and can't) know anything about the 
-                            actual state of the timer - whether it's ticking, 
-                            how much time is left, etc. The only way it can find 
-                            out whether the timer has expired is if it receives 
-                            a message from the timer component. So, the timer's
-                            update function is configured to send a message to
-                            the app's update function when it reaches zero
-                            """
+                        The only thing the app's model does is keep track of
+                        whether the timer has expired. But as you can see, 
+                        it doesn't know anything about the actual state of the 
+                        timer - whether it's ticking, how much time is left, 
+                        etc.
+                        """
                     ]
                 , Html.p []
                     [ Html.text
                         """
-                            Here we have a button in the app's view function 
-                            that can send a message to the timer component
-                            """
+                        The only way the main app can find out whether the timer 
+                        has expired is if it receives a message from the timer 
+                        component. So, the timer's update function is configured 
+                        to send a `TimerExpired` message to the app's update 
+                        function when it reaches zero. It will also send a 
+                        `TimerReset` message if the reset button has been 
+                        clicked.
+                        """
+                    ]
+                , Html.p []
+                    [ Html.text
+                        """
+                        As well as receiving messages from its components, the 
+                        main app can also send messages to them. Here we have a 
+                        button in the main app's view function that sends a 
+                        `Reset` message to the timer component.
+                        """
                     ]
                 , Html.button
-                    [ Html.Events.onClick (timer.toMsg Reset) ]
+                    [ Html.Events.onClick timer.reset ]
                     [ Html.text "Reset timer" ]
                 ]
     , subscriptions =
@@ -122,33 +132,36 @@ timerComponent { timerExpired, timerReset } =
 
                 Tick ->
                     if model == Just 0 then
-                        ( Just 0, Task.perform (\_ -> toApp timerExpired) (Process.sleep 0) )
+                        ( Just 0, send toApp timerExpired )
 
                     else
                         ( Maybe.map (\n -> n - 1) model, Cmd.none )
 
                 Reset ->
-                    ( Nothing, Task.perform (\_ -> toApp timerReset) (Process.sleep 0) )
+                    ( Nothing, send toApp timerReset )
     , interface =
-        \toApp toSelf model -> { toMsg = toSelf, view = 
-            Html.article
-                [ Html.Attributes.style "border" "solid 1px pink"
-                , Html.Attributes.style "border-radius" "10px"
-                , Html.Attributes.style "background-color" "aliceblue"
-                , Html.Attributes.style "padding" "10px"
-                , Html.Attributes.style "width" "200px"
-                , Html.Attributes.style "text-align" "center"
-                , Html.Attributes.style "font-family" "sans-serif"
-                ]
-                [ Html.p [] [ Html.text "Countdown timer" ]
-                , Html.h1 [] [ Html.text (model |> Maybe.withDefault 10 |> String.fromInt) ]
-                , Html.button
-                    [ Html.Events.onClick (toSelf Start) ]
-                    [ Html.text "Start" ]
-                , Html.button
-                    [ Html.Events.onClick (toSelf Reset) ]
-                    [ Html.text "Reset" ]
-                ]}
+        \toApp toSelf model ->
+            { reset = toSelf Reset
+            , view =
+                Html.article
+                    [ Html.Attributes.style "border" "solid 1px pink"
+                    , Html.Attributes.style "border-radius" "10px"
+                    , Html.Attributes.style "background-color" "aliceblue"
+                    , Html.Attributes.style "padding" "10px"
+                    , Html.Attributes.style "width" "200px"
+                    , Html.Attributes.style "text-align" "center"
+                    , Html.Attributes.style "font-family" "sans-serif"
+                    ]
+                    [ Html.p [] [ Html.text "Countdown timer" ]
+                    , Html.h1 [] [ Html.text (model |> Maybe.withDefault 10 |> String.fromInt) ]
+                    , Html.button
+                        [ Html.Events.onClick (toSelf Start) ]
+                        [ Html.text "Start" ]
+                    , Html.button
+                        [ Html.Events.onClick (toSelf Reset) ]
+                        [ Html.text "Reset" ]
+                    ]
+            }
     , subscriptions =
         \toApp toSelf model ->
             case model of
@@ -158,3 +171,7 @@ timerComponent { timerExpired, timerReset } =
                 Just _ ->
                     Time.every 1000 (\_ -> toSelf Tick)
     }
+
+
+send toX msg =
+    Task.perform toX (Task.succeed msg)
