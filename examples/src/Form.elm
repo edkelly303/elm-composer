2b45a6b26b12671139f778f639172afde45013eb
Ed Kelly <edwardjkelly@gmail.com>
2024-10-31 13:35:38 +0000

    Get form example working

--- examples/src/Form.elm
+++ examples/src/Form.elm
@@ -5,30 +5,51 @@ import Composer.Element
 import Html
 import Html.Attributes
 import Html.Events
+import Result.Extra
 import Task
 import Time
 
 
+type alias User =
+    { name : String, age : Int }
+
+
 main =
     Composer.Element.app formApp
-        |> Composer.Element.component string (\toApp appModel -> 1)
-        |> Composer.Element.component string (\toApp appModel -> 2)
-        |> Composer.Element.compose (\a b -> ( a, b ))
+        |> Composer.Element.componentSimple (string "Name")
+        |> Composer.Element.componentSimple (int "Age")
+        |> Composer.Element.compose
+            (\name age ->
+                { name = name
+                , age = age
+                , output =
+                    Ok User
+                        |> Result.Extra.andMap name.parsed
+                        |> Result.Extra.andMap age.parsed
+                }
+            )
         |> Browser.element
 
 
 formApp =
     { init =
-        \( a, b ) toSelf () ->
+        \{ name, age } toSelf () ->
             ( (), Cmd.none )
     , update =
-        \( a, b ) toSelf () () ->
+        \{ name, age } toSelf () () ->
             ( (), Cmd.none )
     , view =
-        \( a, b ) toSelf () ->
-            Html.text "wooo"
+        \{ name, age, output } toSelf () ->
+            Html.form []
+                [ Html.h1 [] [ Html.text "Create a user" ]
+                , name.view
+                , age.view
+                , Html.button
+                    [ Html.Attributes.disabled (Result.Extra.isErr output) ]
+                    [ Html.text "Submit!" ]
+                ]
     , subscriptions =
-        \( a, b ) toSelf () ->
+        \{ name, age } toSelf () ->
             Sub.none
     }
 
@@ -37,23 +58,70 @@ type StringMsg
     = StringChanged String
 
 
-string =
+string label =
     { interface =
-        \toSelf () ->
-            1
+        \toSelf model ->
+            { view = textInputView label toSelf model
+            , parsed = model.parsed
+            }
     , init =
-        \toSelf () ->
-            ( ()
+        \toSelf model ->
+            ( { value = "", parsed = Ok "" }
             , Cmd.none
             )
     , update =
-        \int toSelf () () ->
-            ( (), Cmd.none )
+        \app toSelf (StringChanged str) model ->
+            ( { model | value = str, parsed = Ok str }, Cmd.none )
     , subscriptions =
-        \int toSelf () ->
-            let
-                _ =
-                    int * 2
-            in
+        \app toSelf model ->
             Sub.none
     }
+
+
+int label =
+    { interface =
+        \toSelf model ->
+            { view = textInputView label toSelf model
+            , parsed = model.parsed
+            }
+    , init =
+        \toSelf model ->
+            ( { value = "", parsed = Err "Must be an integer" }
+            , Cmd.none
+            )
+    , update =
+        \app toSelf (StringChanged str) model ->
+            ( { model
+                | value = str
+                , parsed = String.toInt str |> Result.fromMaybe "Must be an integer"
+              }
+            , Cmd.none
+            )
+    , subscriptions =
+        \app toSelf model ->
+            Sub.none
+    }
+
+
+textInputView label toSelf { value, parsed } =
+    let
+        ( icon, message ) =
+            case parsed of
+                Ok p ->
+                    ( "✅", "" )
+
+                Err e ->
+                    ( "🚫 ", e )
+    in
+    Html.div []
+        [ Html.label []
+            [ Html.strong [] [ Html.text label ]
+            , Html.div []
+                [ Html.input
+                    [ Html.Events.onInput (toSelf << StringChanged) ]
+                    [ Html.text value ]
+                , Html.text icon
+                ]
+            , Html.small [] [ Html.text message ]
+            ]
+        ]
